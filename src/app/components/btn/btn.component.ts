620df7953bcbb9bbcd02dc2e692f7a8b5458aea0
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-19 16:39:31 -0500

    aplicacion de dinamismo en clases bajo ngclass

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   templateUrl: './btn.component.html',
-  styleUrls: ['./btn.component.scss']
+  styleUrls: ['./btn.component.scss'],
 })
 export class BtnComponent implements OnInit {
+  @Input() typeBtn: 'button' | 'submit' | 'reset' = 'button';
+  @Input() color = 'primary';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get colors() {
+    return {
+      'hover:bg-green-600': this.color === 'green',
+      'bg-green-700': this.color === 'green',
+      'focus:ring-green-300': this.color === 'green',
+      'hover:bg-primary-600': this.color === 'primary',
+      'bg-primary-700': this.color === 'primary',
+      'focus:ring-primary-300': this.color === 'primary',
+    };
   }
-
 }
