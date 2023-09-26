21ef8294afcb994c8afc5767653fc7a7b8ff90ab
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-26 17:01:25 -0500

    estilizado del componente navbar y anexo de estilos del cdk al archivo styles.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 
 const routes: Routes = [
   {
-  path: 'login',
-  component: LoginComponent
-  }
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'boards',
+    component: BoardsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
