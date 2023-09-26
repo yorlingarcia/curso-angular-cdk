8d6b23dba1ab275e0d79304f9ef453265415c300
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-26 16:48:53 -0500

    Creacion del component navbar y la pagina board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BtnComponent
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
