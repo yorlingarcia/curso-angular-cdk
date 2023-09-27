d17d876f31383d5e886471fa9fd5aad2098bdb34
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-27 16:45:30 -0500

    estilizado para la pagina board

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faTable,
+  faTabletAlt,
+  faWaveSquare,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
-  templateUrl: './boards.component.html'
+  templateUrl: './boards.component.html',
 })
 export class BoardsComponent implements OnInit {
+  faTable = faTable;
+  faTabletAlt = faTabletAlt;
+  faWaveSquare = faWaveSquare;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
