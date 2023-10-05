76c9bf0e0aa7bcd754d770136b936d6e7b17e8e5
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-10-04 19:42:55 -0500

    creacion de modelo y cambio en la lista de elementos de los todos

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,14 +1,32 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { ToDo } from 'src/app/models/todo.model';
 
 @Component({
   selector: 'app-board',
-  templateUrl: './board.component.html'
+  templateUrl: './board.component.html',
 })
 export class BoardComponent implements OnInit {
+  toDos: ToDo[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  drop($event: CdkDragDrop<any[]>) {
+    console.log($event);
+    // moveItemInArray();
   }
-
 }
