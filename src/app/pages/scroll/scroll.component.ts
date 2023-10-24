e3d2e3fbff07e69e5e517be21b96d0a52f207d85
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-10-24 18:36:22 -0500

    Consumo de api de platzi

--- src/app/pages/scroll/scroll.component.ts
+++ src/app/pages/scroll/scroll.component.ts
@@ -1,14 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+interface Product {
+  id: string;
+  title: string;
+  images: string[];
+  price: number;
+}
 
 @Component({
   selector: 'app-scroll',
-  templateUrl: './scroll.component.html'
+  templateUrl: './scroll.component.html',
 })
 export class ScrollComponent implements OnInit {
-
-  constructor() { }
+  productos: Product[] = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => (this.productos = data));
   }
-
 }
