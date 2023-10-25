671cd03e69d17ace4efe20d4c422dfea12041da5
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-10-25 15:44:25 -0500

    Cambio hacia codificacion reactiva

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,19 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+import { DataSourceProduct } from './data-source';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
 })
 export class TableComponent implements OnInit {
-  productos: Product[] = [];
+  dataSource = new DataSourceProduct();
   columns: string[] = ['id', 'title', 'price', 'image'];
+  total = 0;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
       .get<Product[]>('https://api.escuelajs.co/api/v1/products')
-      .subscribe((data) => (this.productos = data));
+      .subscribe((data) => {
+        this.dataSource.init(data), (this.total = this.dataSource.getTotal());
+      });
   }
 }
