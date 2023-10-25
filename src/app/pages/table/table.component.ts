32030adc68bd28a6077131c5a6cadfb0328aea89
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-10-24 21:52:21 -0500

    creacion de un nuevo modelo de products, uso del routin y estilizado del table

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-table',
-  templateUrl: './table.component.html'
+  templateUrl: './table.component.html',
 })
 export class TableComponent implements OnInit {
-
-  constructor() { }
+  productos: Product[] = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => (this.productos = data));
   }
-
 }
